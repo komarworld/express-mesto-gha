04589da149e2b683bf0429983438e43057f351b0
Bella <mytodolist@yandex.ru>
2023-06-22 23:46:04 +0300

    PR13

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,14 @@ const router = require('express').Router();
 const userRoutes = require('./users');
 const cardRoutes = require('./cards');
 
+const { NOT_FOUND_ERROR } = require('../utils/utils');
+
 router.use('/users', userRoutes);
 router.use('/cards', cardRoutes);
+
+router.use((req, res) => {
+  res.status(NOT_FOUND_ERROR).send({ message: 'Страница не найдена' });
+});
 
 
 module.exports = router;
