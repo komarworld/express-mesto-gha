702b87c6d4ca6319697c35d7c5a2fec1e04e54ac
Bella <mytodolist@yandex.ru>
2023-07-09 00:40:11 +0300

    PR 14 1

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,7 +4,7 @@ const JWT_SECTER = 'super-secret-key';
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-console.log(req.headers);
+  console.log(req.headers);
   if (!authorization || !authorization.startsWith('Bearer ')) {
     return res.status(401).send({ message: 'Необходима авторизация' });
   }
